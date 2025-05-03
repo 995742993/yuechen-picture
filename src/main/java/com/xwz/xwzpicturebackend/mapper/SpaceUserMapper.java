b640f3730afbf6b44baa52d005b9e55eb9b71956
yuelu <995742993@qq.com>
2025-05-03 21:55:18 +0800

    团队空间模块相关代码-数据实体创建

---
+++ src/main/java/com/xwz/xwzpicturebackend/mapper/SpaceUserMapper.java
@@ -0,0 +1,19 @@
+package com.xwz.xwzpicturebackend.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xwz.xwzpicturebackend.domain.entity.SpaceUser;
+
+/**
+* @author yuelu
+* @description 针对表【space_user(空间用户关联)】的数据库操作Mapper
+* @createDate 2025-05-02 16:38:13
+* @Entity generator.domain.SpaceUser
+*/
+public interface SpaceUserMapper extends BaseMapper<SpaceUser> {
+
+}
+
+
+
+
